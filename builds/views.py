c0a5b49d9b9081b5d36888d1cbd65ac647989b3c
Jon Turney <jon.turney@dronecode.org.uk>
2016-04-13 23:42:26 +0100

    Add CSS and pagination

--- builds/views.py
+++ builds/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render
 from django.http import HttpResponse
 
@@ -6,7 +7,17 @@ from builds.models import Jobs
 import os
 
 def index(request):
-  rows = Jobs.objects.using('builds').all().order_by('-id')
+  all_rows = Jobs.objects.using('builds').all().order_by('-id')
+  paginator = Paginator(all_rows, 25)
+
+  page = request.GET.get('page')
+  try:
+     rows = paginator.page(page)
+  except PageNotAnInteger:
+     rows = paginator.page(1)
+  except EmptyPAge:
+     rows = paginator.page(paginator.num_pages)
+
   return render(request, "builds/template.html", {"rows" : rows })
 
 def log(request, fn):
